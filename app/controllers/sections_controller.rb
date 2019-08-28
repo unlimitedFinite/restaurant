5d40271131d2e4e304334355c35e9f532189eb73
Daniele Deltodesco <deaddread666@gmail.com>
2019-08-28 13:33:25 +0100

    list can now be sorted manually

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -25,6 +25,11 @@ class SectionsController < ApplicationController
     @section = Section.find(params[:id])
     @dish = Dish.find(params[:section][:dish_ids])
     @clone = @dish.dup
+    if (l = @section.dishes.last)
+      @clone.sort = l.sort + 1
+    else
+      @clone.sort = 1
+    end
     if @section.dishes << @clone
       redirect_to menu_path(params[:menu_id])
     else
