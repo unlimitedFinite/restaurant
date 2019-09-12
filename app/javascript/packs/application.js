407886a0cf242a9be364ef2714144ec2c61fee64
Daniele Deltodesco <deaddread666@gmail.com>
2019-09-12 20:48:55 +0800

    changed bootstrap config in packs/application.js and css/application.css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("bootstrap/dist/js/bootstrap")
 
-import 'bootstrap'
-import './src/application.scss'
+// import 'bootstrap'
+// import './src/application.scss'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
